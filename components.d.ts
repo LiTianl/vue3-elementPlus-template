a22990625d48dec4ebbe1f51ab9189d220ae9f56
lishenglll <3330145207l@sina.com>
2023-03-14 21:40:56 +0800

    1

--- components.d.ts
+++ components.d.ts
@@ -13,5 +13,6 @@ declare module '@vue/runtime-core' {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
   }
 }
