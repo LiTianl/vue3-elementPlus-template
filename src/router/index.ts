9062633081187a608b4c2f5a97ec74f0db509388
添乐 李 <LT3333333@outlook.com>
2023-03-16 17:18:55 +0800

    部分修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import { error, home, user } from './modules'
 
+export const constanRouters = [...home, ...user]
+
+export const asyncRouters = []
+
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    ...home,
-    ...user
-  ]
+  routes: constanRouters
 })
 
 export default router
