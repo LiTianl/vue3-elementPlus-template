df338969888828861b5679d92330b8990a474eef
lishenglll <3330145207l@sina.com>
2023-03-17 09:44:12 +0800

    合并冲突

--- components.d.ts
+++ components.d.ts
@@ -12,8 +12,6 @@ declare module '@vue/runtime-core' {
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
-    ElButton: typeof import('element-plus/es')['ElButton']
-
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <router-view />
 </template>
 
-<style scoped>
+<style>
 html,
 body,
 #app {
@@ -13,16 +13,5 @@ body,
   padding: 0;
   height: 100%;
 }
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+
 </style>
