9090798d6855269d7737d572210b2f9864c3305c
添乐 李 <LT3333333@outlook.com>
2023-03-30 14:40:03 +0800

    添加并完善logo

--- src/layout/components/sidebar/logo.vue
+++ src/layout/components/sidebar/logo.vue
@@ -1,7 +1,40 @@
 <script setup lang='ts'>
+import { inject } from 'vue'
+const collapse = inject('collapse')
 </script>
 <template>
-<div class="logo">logo</div>
+  <router-link to='/' class="logo" >
+    <div class="icon">
+      <svg-icon icon-class="Vector" />
+    </div>
+    <div class="title" :class="collapse ? 'collapse' : ''">
+      Vue Template
+    </div>
+  </router-link>
 </template>
 <style scoped lang='scss'>
+.logo{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  background-color: #2b2f3a;
+  text-decoration: none;
+  .icon{
+    svg{
+      width:32px;
+      height:32px;
+    }
+  }
+  .title{
+    font-weight: 700;
+    color: white;
+    padding: 0 15px;
+    white-space: nowrap;
+  }
+  .title.collapse{
+    display: none
+  }
+}
+
 </style>
